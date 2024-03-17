25556cf5b395b40ba6238ad01d61e33c8845f838
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-17 19:34:57 +0530

    jwt token

---
+++ helpers/generateToken.js
@@ -0,0 +1,13 @@
+
+import JWT from 'jsonwebtoken'
+
+export const generateToken = (user) =>{
+    try {
+        
+        const token =  JWT.sign({_id : user._id} , process.env.JWT_SECRET , {expiresIn : process.env.JWT_EXPIRES})
+        return token
+
+    } catch (error) {
+        console.log('Error generating token' , error);
+    }
+}
