b0b0c249155d48f43be935ba040b4abb65ff7c09
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-19 13:35:06 +0530

    userProfile & logout

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import { getCurrentUser, loginUser, logoutUser, registerUser } from '../controllers/authController.js';
+import { isAuthenticated } from '../middlewares/isAuthenticate.js';
 
 const authRouter = express.Router();
 
 // @route   POST api/auth
 authRouter.post('/register' , registerUser)
 authRouter.post('/login' , loginUser)
+authRouter.get('/current-user' , isAuthenticated , getCurrentUser)
+authRouter.get('/logout' , isAuthenticated , logoutUser)
 
 export default authRouter
