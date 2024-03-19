0a5d338c4911710b5f2c2f0787d8914518a1d7b7
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-19 13:33:30 +0530

    update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,45 @@
-import { handleCastError, handleCatchError , handleValidationError} from "../helpers/handleError.js"
+import { handleCastError, handleCatchError, handleValidationError } from "../helpers/handleError.js"
 import UserModel from "../models/userModel.js"
 
 
 export const getSingleUser = async (req, res) => {
     try {
-        const user = await UserModel.findById(req.params.id , {password : 0})
-        if(!user){
-            return handleValidationError(res , "User not found" , 404)
+        const user = await UserModel.findById(req.params.id, { password: 0 })
+        if (!user) {
+            return handleValidationError(res, "User not found", 404)
         }
         res.status(200).json({
-            success : true,
-            message : `${user.userName} profile fetched`,
-            user : user
+            success: true,
+            message: `${user.userName} profile fetched`,
+            user: user
         })
 
 
     } catch (error) {
         if (error.name === 'CastError') {
             return handleCastError(res, 'Invalid Id');
-          }
-        // handleCatchError(res , "Error while getting the single user" , error)
-        // Custom status code
-        handleCatchError(res, 'Error registering user', error, 500); 
+        }
+        handleCatchError(res, 'Error registering user', error, 500);
 
     }
 }
+
+
+export const updateUser = async (req, res) => {
+    try {
+        // console.log(req.user);
+        const user = await UserModel.findByIdAndUpdate(req.user._id, req.body, { new: true }).select('-password');
+
+        res.status(200).json({
+            success: true,
+            message: "user updated successfully",
+            user: user
+        })
+    } catch (error) {
+        if (error.name === 'CastError') {
+            return handleCastError(res, 'Invalid Id')
+        }
+        handleCatchError(res, 'Error updating user', error, 500);
+    }
+}
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getSingleUser } from '../controllers/userController.js';
+import { followUser, getSingleUser, updateUser } from '../controllers/userController.js';
 import { isAuthenticated } from '../middlewares/isAuthenticate.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/get/:id' , isAuthenticated , getSingleUser)
+userRouter.put('/update' , isAuthenticated , updateUser)
+
 
 
 export default userRouter
