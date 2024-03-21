00b2eab27316040040c3473bfd3290174b42536b
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-22 00:42:04 +0530

    create post with images

--- controllers/postController.js
+++ controllers/postController.js
@@ -23,7 +23,7 @@ export const createPost = async (req, res) => {
         user.posts.push(newPost._id)
         await user.save()
         
-        res.status(200).json({
+        res.status(201).json({
             success: true,
             message: "Post created successfully", 
             post : newPost
@@ -35,6 +35,54 @@ export const createPost = async (req, res) => {
             return handleCastError(res, 'Invalid Id');
         }
         handleCatchError(res, 'Error while creaing the post', error, 500);
+
+    }
+}
+
+// generating the file Url
+const generateFileUrl = (filename) =>{
+    return process.env.URL+`/uploads/${filename}`
+}
+
+export const createPostWithImages = async (req, res) => {
+    try {
+        const { caption } = req.body
+        const {_id} = req.user
+        const files = req.files
+
+        console.log(caption);
+        const user = await UserModel.findById(_id)
+        if (!user) {
+            return handleValidationError(res, "User not found", 404)
+        }
+        
+       //getting the imageUrl array from files
+       const imageUrls = files.map(file => generateFileUrl(file.filename))
+
+       //creatin the new post
+       const newPost = await PostModel.create({
+        user : _id,
+        caption : caption,
+        image : imageUrls
+       })
+
+       console.log(newPost);
+
+        // pushing the posts to the user posts array
+        user.posts.push(newPost._id)
+        
+        res.status(201).json({
+            success: true,
+            message: "Post created successfully along with images", 
+            post : newPost
+        })
+
+
+    } catch (error) {
+        if (error.name === 'CastError') {
+            return handleCastError(res, 'Invalid Id');
+        }
+        handleCatchError(res, 'Error while creaing the post with images', error, 500);
 
     }
 }
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import upload from '../middlewares/upload.js';
 import { isAuthenticated } from '../middlewares/isAuthenticate.js';
-import { createPost } from '../controllers/postController.js';
+import { createPost, createPostWithImages } from '../controllers/postController.js';
 
 
 const postRouter = express.Router();
 
-postRouter.post('/create' , isAuthenticated , createPost)
+//create post 
+postRouter.post('/create', isAuthenticated, createPost)
+
+
+//create post with images
+postRouter.post('/create-with-image', isAuthenticated, upload.array("images", 5), createPostWithImages)
 
 export default postRouter
