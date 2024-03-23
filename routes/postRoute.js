ab3872bbfa870c73ba0145c4e20320ca140407a3
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-23 14:32:38 +0530

    updated

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -9,30 +9,23 @@ const postRouter = express.Router();
 //create post 
 postRouter.post('/create', isAuthenticated, createPost)
 
-
 //create post with images
 postRouter.post('/create-with-image', isAuthenticated, upload.array("images", 5), createPostWithImages)
-
 
 // update post 
 postRouter.put('/update/:postId', isAuthenticated, updatePost)
 
-
 // get all posts
 postRouter.get('/get-all/:userId' , isAuthenticated , getAllPosts)
-
 
 // get user posts
 postRouter.get('/get-user-posts' , isAuthenticated , getUserPosts)
 
-
 // delete post
 postRouter.delete('/delete/:postId' , isAuthenticated , deletePost)
 
-
 // like post
 postRouter.post('/like/:postId' , isAuthenticated , likePost)
-
 
 // Unlike post
 postRouter.post('/un-like/:postId' , isAuthenticated , UnlikePost)
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,38 +11,29 @@ userRouter.get('/get/:id' , isAuthenticated , getSingleUser)
 //update user
 userRouter.put('/update' , isAuthenticated , updateUser)
 
-
 // which user we want to follow so mentioned in the req.params(userId)
 userRouter.post('/follow/:userId' , isAuthenticated , followUser )
-
 
 // which user we want to un-follow so mentioned in the req.params(userId)
 userRouter.post('/unfollow/:userId' , isAuthenticated , unfollowUser )
 
-
 // which user we want to block so mentioned in the req.params(userId)
 userRouter.post('/block/:userId' , isAuthenticated , blockUser )
-
 
 // which user we want to un-block so mentioned in the req.params(userId)
 userRouter.post('/unblock/:userId' , isAuthenticated , unblockUser )
 
-
 // GET BLOCK USERS LISTS
 userRouter.get('/blocked-lists' , isAuthenticated , getBlockLists )
-
 
 //delete user
 userRouter.delete('/delete' , isAuthenticated , deleteUser)
 
-
 //serch user
 userRouter.get('/search/:query' , isAuthenticated , searchUser)
 
-
 //update profile picture
 userRouter.put('/update-profile-picture' , isAuthenticated , upload.single("profilePicture" ), uploadProfilePicture)
-
 
 //update cover picture
 userRouter.put('/update-cover-picture' , isAuthenticated , upload.single("coverPicture" ), uploadProfileCoverPicture)
